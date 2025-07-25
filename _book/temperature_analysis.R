8cd883007949cac466b0c2713319d39d7479d998
mfbenitezp <fbenitez@turing.ac.uk>
2025-07-25 10:37:31 +0100

    last edits

---
+++ _book/temperature_analysis.R
@@ -0,0 +1,18 @@
+# Urban Heat Island Analysis
+# GG3209 - Spatial Data Science
+
+# Load required packages
+library(sf)
+library(terra)
+library(ggplot2)
+library(dplyr)
+
+# Read temperature data
+# temperature_data <- rast("data/raw/landsat_heat.tif")
+
+# Read city boundary
+# city_boundary <- st_read("data/raw/city_boundary.shp")
+
+# TODO: Add temperature extraction analysis
+# TODO: Add spatial interpolation
+# TODO: Create temperature maps
